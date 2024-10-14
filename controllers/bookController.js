831ed764a5289a52da68929bf8e9d090ae94975f
sesdevAchary <sesdev.achary@gmail.com>
2024-10-14 10:59:23 +0000

    crud app with MongodB

--- controllers/bookController.js
+++ controllers/bookController.js
@@ -1,14 +1,28 @@
 const bookModel = require('../models/bookModel')
 
 
+// CREATING A BOOK ....................................................//
+
+
 /* createa a fun , exported as a module*/
 exports.createBook = async(req,res)=>{
 
+    try{
 /* creating a new book instance(which is a mongoose model), representing a book in db*/
 let singleBook = new bookModel({title:req.body.title, auhtor:req.body.auhtor});
 
 // saving the book to the database //
 /* await key pauses exec until the save op is complete and returns the saved doc , stored in singleBook*/
 singleBook = await singleBook.save();
+res.send(singleBook);
+
+} catch(error){
+    res.status(400).send(error.message)
+}
+
+
 
 }
+
+
+//  GETTING ALL THE BOOKS...........................................................................................//
