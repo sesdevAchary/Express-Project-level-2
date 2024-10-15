08cd045a555f18e89d07ac2c6878095599f4763e
sesdevAchary <sesdev.achary@gmail.com>
2024-10-15 06:00:15 +0000

    crud app with MongodB

---
+++ routes/bookRoutes.js
@@ -0,0 +1,9 @@
+import express from "express"
+const router = express.Router();
+import bookController from './controllers/bookController.js'
+
+router.post('/books' , bookController.createbook);
+router.get('/books' , bookController.getAllBooks);
+router.get('/books/:id' , bookController.getBookById);
+router.put('/books/:id' , bookController.updateBookById);
+router.delete('/books/:id' , bookController.deleteBookById);
